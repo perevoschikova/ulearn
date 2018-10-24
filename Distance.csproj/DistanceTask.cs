b5bcca0a3fe242620c0abbdc17e01739b670e3b5
perevoschikova <katyakur13@gmail.com>
2018-10-24 09:23:25 +0300

    Правлю вторую версию решения задачи с дистанцией

--- Distance.csproj/DistanceTask.cs
+++ Distance.csproj/DistanceTask.cs
@@ -8,13 +8,13 @@ namespace DistanceTask
         // Расстояние от точки (x, y) до отрезка AB с координатами A(ax, ay), B(bx, by)
         public static double GetDistanceToSegment(double ax, double ay, double bx, double by, double x, double y)
         {
-            double ak = Math.Sqrt((x - ax) * (x - ax) + (y - ay) * (y - ay));
-            double kb = Math.Sqrt((x - bx) * (x - bx) + (y - by) * (y - by));
-            double ab = Math.Sqrt((ax - bx) * (ax - bx) + (ay - by) * (ay - by));
+            var ak = Math.Sqrt((x - ax) * (x - ax) + (y - ay) * (y - ay));
+            var kb = Math.Sqrt((x - bx) * (x - bx) + (y - by) * (y - by));
+            var ab = Math.Sqrt((ax - bx) * (ax - bx) + (ay - by) * (ay - by));
 
             //скалярное произведение векторов
-            double mulScalarAKAB = (x - ax) * (bx - ax) + (y - ay) * (by - ay);
-            double mulScalarBKAB = (x - bx) * (-bx + ax) + (y - by) * (-by + ay);
+            var mulScalarAKAB = (x - ax) * (bx - ax) + (y - ay) * (by - ay);
+            var mulScalarBKAB = (x - bx) * (-bx + ax) + (y - by) * (-by + ay);
 
             if (ab == 0)
             {
@@ -23,18 +23,18 @@ namespace DistanceTask
 
             if (mulScalarAKAB >= 0 && mulScalarBKAB >= 0)
             {
-                double p = (ak + kb + ab) / 2.0;
-                double s = Math.Sqrt(Math.Abs((p * (p - ak) * (p - kb) * (p - ab))));
+                var p = (ak + kb + ab) / 2.0;
+                var s = Math.Sqrt(Math.Abs((p * (p - ak) * (p - kb) * (p - ab))));
 
                 return (2.0 * s) / ab;
             }
 
-            else if (mulScalarAKAB < 0 || mulScalarBKAB < 0)
+            if (mulScalarAKAB < 0 || mulScalarBKAB < 0)
             {
                 return Math.Min(ak, kb);
             }
 
-            else return 0;
+            return 0;
         }
     }
 }
